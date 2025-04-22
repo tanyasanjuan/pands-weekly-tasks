4fa5f7dac32ea1a8c8332f6187270bc25634957d
Tanya <tanya.sanjuan@gmail.com>
2025-04-22 23:34:15 +0100

    Adding comments and explanation of print function.

--- Week01/helloworld.py
+++ Week01/helloworld.py
@@ -1,1 +1,17 @@
+# This is a simple Python program that prints "Hello World" to the console.
+# Author: Tanya San Juan.
+
+# To have a file in python format, we have to save the file as .py
+# To display the text in the console we have to add the "print()" function.
+# The "print()" function take any parameter and displays them in a text line in the console(terminal)/screen.
+# The messages inside of the print can be a string(text) 
+# or any other object that will be converted in a string before to display.
+
 print ("Hello World")
+
+# To run the program, type the command: python (name of the file) in the console.
+# In this case: python helloword.py
+
+# Sources: 
+# https://www.w3schools.com/python/ref_func_print.asp
+# https://cs.stanford.edu/people/nick/py/python-print.html
