0fada2274e496e22a985cff660353c488507eafe
Tanya <tanya.sanjuan@gmail.com>
2025-04-22 17:33:35 +0100

    Create the plot size, using plt figure

--- Week08/plottask.py
+++ Week08/plottask.py
@@ -1,5 +1,5 @@
 # This program displays:
-# - A histogram of a normal distrbution of a 1000 values 
+# - A histogram of a normal distribution of a 1000 values 
 # with a mean of 5 and stadard deviation of 2,
 # - and a plot of the function h(x) = x^3 in the range 0 to 10.
 # on the one set of axes.
@@ -29,3 +29,10 @@ x = np.linspace(0, 10, 100)
 # The function h(x) = x^3 is a cubic function. And it's calculated by raising x to the power of 3.
 # calculates h(x) = x^3 for each value.
 h_x = x**3
+
+
+# The plot
+# plt.figure(figsize) : Create a figure with a specified size (10 inches wide x 5 inches tall)
+# Source: https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.figure.html
+plt.figure(figsize=(10, 5)) # Set the figure size to 10x5 inches.
+
